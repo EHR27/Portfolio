ec88b61baf688d7db316e956b9439fa1d011d205
Edward <ehr1360@hotmail.com>
2024-03-18 11:43:15 -0400

    Fixed link of LinkedIn

--- data.js
+++ data.js
@@ -55,6 +55,6 @@ const informacionPersonal ={
     ],
     redes: [
         ["github", "https://github.com/EHR27"],
-        ["linkedin", "www.linkedin.com/in/edward-hernandez-ramirez-b07490257/"],
+        ["linkedin", "https://www.linkedin.com/in/edward-hernandez-ramirez-b07490257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"],
     ]
 };
